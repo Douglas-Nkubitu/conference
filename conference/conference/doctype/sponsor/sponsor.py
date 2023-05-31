92e5c6ec3ae76ff51f4a09d36f076228ed7fe398
Douglas <nkubitudouglas@gmail.com>
2023-05-31 11:23:18 +0300

    feat: Initialize App

--- conference/conference/doctype/sponsor/sponsor.py
+++ conference/conference/doctype/sponsor/sponsor.py
@@ -1,8 +1,17 @@
 # Copyright (c) 2023, Douglas Nkubitu and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import validate_email_address, validate_phone_number
 
 class Sponsor(Document):
-	pass
+	def validate(self):
+		self.validate_email()
+		self.validate_mobile_no()
+
+	def validate_mobile_no(self):
+		validate_phone_number(self.mobile_no.strip(), True)
+
+	def validate_email(self):
+		validate_email_address(self.email_id.strip(), True)
