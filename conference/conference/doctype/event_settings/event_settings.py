96c6dbf96322ad5c5cdb4e116f1ab187ed10de34
Douglas <nkubitudouglas@gmail.com>
2023-05-31 11:41:15 +0300

    feat: Initialize App

--- conference/conference/doctype/event_settings/event_settings.py
+++ conference/conference/doctype/event_settings/event_settings.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
-class EventsSetup(Document):
+class EventSettings(WebsiteGenerator):
 	pass
--- conference/conference/doctype/event_settings/test_event_settings.py
+++ conference/conference/doctype/event_settings/test_event_settings.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestEventsSetup(FrappeTestCase):
+class TestEventSettings(FrappeTestCase):
 	pass
